0243aa27282b27a5e0d9764612f0db15f5c43d43
= <=>
2020-06-27 23:15:14 +0500

    Add jsx to project and React-specific ESLint config

---
+++ src/Pet.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Pet = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+    </div>
+  );
+};
+
+export default Pet;
--- src/app.js
+++ src/app.js
@@ -1,32 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Pet from "./Pet.jsx";
 
-const pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.age),
-  ]);
+const App = () => {
+  return (
+    <div className="adopt-me">
+      <h1>Adopt Me!</h1>
+      <Pet name="jack" age="10" />
+      <Pet name="ace" age="11" />
+      <Pet name="tiken" age="12" />
+    </div>
+  );
 };
+export default App;
 
-const app = () => {
-  return React.createElement("div", { id: "pet" }, [
-    React.createElement(pet, {
-      name: "jack",
-      age: 10,
-      breed: "german shepard",
-    }),
-    React.createElement(pet, { name: "ace", age: 11, breed: "german shepard" }),
-    React.createElement(pet, {
-      name: "tiken",
-      age: 12,
-      breed: "german shepard",
-    }),
-    React.createElement(pet, {
-      name: "tiger",
-      age: 13,
-      breed: "german shepard",
-    }),
-  ]);
-};
-ReactDOM.render(React.createElement(app), document.getElementById("root"));
-export default app;
+ReactDOM.render(<App />, document.getElementById("root"));
